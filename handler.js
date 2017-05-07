006500a53afe886a11fa111c244b2ff3c5d4066d
Wojtek Przechodzeń <wprzecho@gmail.com>
2017-05-07 17:29:08 +0200

    add package json, improve http request with request-promise

--- handler.js
+++ handler.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const request = require('request-promise');
+
 module.exports.hello = (event, context, callback) => {
     sendMessageToSlackChannel('Hello from Lambda! :tada:')
         .then(() => callback(null, 'OK'))
@@ -8,49 +10,26 @@ module.exports.hello = (event, context, callback) => {
 
 function sendMessageToSlackChannel(message) {
 
-    const requestData = JSON.stringify({
+    const requestData = {
         text: message
-    });
+    };
 
-    return makeHttpsRequest({
+    var options = {
         method: 'POST',
-        url: process.env.SLACK_WEBHOOK,
-        data: requestData,
+        uri: process.env.SLACK_WEBHOOK,
+        body: requestData,
+        json: true,
         headers: {
             'Content-Type': 'application/json',
             'Content-Length': requestData.length
         }
-    });
-}
+    };
 
-function makeHttpsRequest({method, url, headers = {}, data}) {
-
-    return new Promise((resolve, reject) => {
-
-        const parsedUrl = require('url').parse(url);
-        const nativeHttpLibrary = parsedUrl.protocol === 'https:' ? require('https') : require('http');
-
-        const request = nativeHttpLibrary.request({
-            hostname: parsedUrl.hostname,
-            port: parsedUrl.protocol === 'https:' ? 443 : 80,
-            path: parsedUrl.path,
-            method: method,
-            headers: headers
-        }, response => {
-            response.setEncoding('utf8');
-            let responseBody = '';
-            response.on('error', (error) => reject(error));
-            response.on('data', (chunk) => responseBody = responseBody + chunk);
-            response.on('end', () => resolve({
-                status: response.statusCode,
-                body: responseBody
-            }));
+    return request(options)
+        .then(function (parsedBody) {
+            console.log('Wszystko porzło dobrze');
+        })
+        .catch(function (err) {
+            console.log(err);
         });
-
-        request.on('error', (error) => reject(error));
-        if (data) {
-            request.write(data);
-        }
-        request.end();
-    });
 }
